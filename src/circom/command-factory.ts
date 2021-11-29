4590fe2c5f29d20a8bc98c4f809746e693232c5a
Heng Zhang <shindar90@gmail.com>
2021-11-30 01:26:46 +0800

    add placeholder command

--- src/circom/command-factory.ts
+++ src/circom/command-factory.ts
@@ -1,10 +1,13 @@
 import { Field } from "delphinus-curves/src/field";
 import { AddPoolCommand } from "./ops/addpool";
 import { CommandOp } from "delphinus-l2-client-helper/src/swap";
+import { Command } from "./command";
 
 export function createCommand(op: Field, args: Field[]) {
   if (op.v.eqn(CommandOp.AddPool)) {
     return new AddPoolCommand(args);
+  } else {
+    return new Command(args);
   }
 
   throw new Error('Not implemented yet');
