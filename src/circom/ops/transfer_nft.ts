743b4d41d4fc3aba4f658da65c388e7d2db245a3
akayi07 <zhuolimiaotx@163.com>
2021-12-28 00:45:53 +0800

    add transfer_nft

---
+++ src/circom/ops/transfer_nft.ts
@@ -0,0 +1,43 @@
+import { Field } from "delphinus-curves/src/field";
+import { PathInfo } from "delphinus-curves/src/merkle-tree-large";
+import { L2Storage } from "../address-space";
+import { Command } from "../command";
+import { Account } from "../address/account";
+import { NFT } from "../address/nft"
+
+// change owner from current acount to another
+export class TransferNFTCommand extends Command {
+  get callerAccountIndex() {
+    return this.args[9].v.toNumber();
+  }
+
+  async run(storage: L2Storage) {
+    const path = [] as PathInfo[];
+
+    const nonce = this.args[3];
+    const owner = this.args[4];
+    const bidder = this.args[5];
+    const biddingAmount = this.args[6];
+
+    const nftIndex = this.args[8];
+    const nft = new NFT(storage, nftIndex);
+    const account = new Account(storage, this.callerAccountIndex);
+
+    // circuits: check leafValues[0] < 2 ^ 20 & leafValues[0] != 0
+    // circuits: check leafValues[1]-leafValues[3] is 0
+    // circuits: check owner < 2 ^ 20 & owner != 0
+    // circuits: check leafValues[0] != owner
+    // circuits: check if bidder, biddingAmount and args[7] is 0
+    // circuits: check if sender is current owner(account == leafValues[0])
+    const leafValues = await storage.getLeaves(nft.info_index);
+
+    // STEP1: udpate nonce
+    // circuits: check nonce
+    path.push(await account.getAndUpdateNonce(nonce));
+    
+    // STEP2: update nft info
+    path.push(await nft.getAndUpdateNFT(owner, bidder, biddingAmount));
+
+    return path;
+  }
+}
