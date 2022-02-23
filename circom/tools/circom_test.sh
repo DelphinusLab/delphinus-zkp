51f4ea3a353a23525d1253cf7fe7ae1c20df9852
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-02-23 16:31:04 +1100

    circomtest.sh

--- circom/tools/circom_test.sh
+++ circom/tools/circom_test.sh
@@ -4,3 +4,4 @@ node ../../dist/tests/circom.test.js
 mv ../input.setkey_account1_owner1.json ../unit_tests/setkey/input.json
 mv ../input.addpool_account1_poolindex2.json ../unit_tests/addpool/input.json
 rm ../input.json
+bash unit_run_full.sh ${1}
