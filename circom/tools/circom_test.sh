16c92df04b41fd878185f0fed57013efaaae7b18
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-03-10 18:07:35 +1100

    update unittest tools

--- circom/tools/circom_test.sh
+++
@@ -1,7 +0,0 @@
-npx tsc
-node ../../dist/tests/generateSignPubKey.js
-node ../../dist/tests/circom.test.js
-mv ../input.setkey_account1_owner1.json ../unit_tests/setkey/input.json
-mv ../input.addpool_account1_poolindex2.json ../unit_tests/addpool/input.json
-rm ../input.json
-bash unit_run_full.sh ${1}
