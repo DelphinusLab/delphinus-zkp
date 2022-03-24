0da6de7b9252916dd6da221b6b34a3de9b2650bd
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-03-25 10:59:33 +1100

    fmt

--- circom/tools/UnitTestInputGenerator/Circom.test.ts
+++ circom/tools/UnitTestInputGenerator/Circom.test.ts
@@ -11,7 +11,7 @@ const config = fs.readJsonSync(process.argv[2]);
 let cryptoUtil: CryptoUtil;
 let cryptoUtilPromise = import(
   __dirname + "/../../../../../crypto-rust/node/pkg/delphinus_crypto"
-  ).then((module) => {
+).then((module) => {
   cryptoUtil = module;
   return module;
 });
@@ -25,32 +25,32 @@ async function getCryptoUtil() {
 
 //generate test Input
 async function main() {
-    await storage.startSnapshot("1");
+  await storage.startSnapshot("1");
 
-    //nonce
-    interface nonce {
-        [key: string]: any
+  //nonce
+  interface nonce {
+    [key: string]: any
+  }
+  const nonce_signer: nonce = {};
+
+  //crypto
+  const util = await getCryptoUtil();
+
+  //pre-test
+  console.log('Compiling Circom');
+  await CreateResultFile();
+  await preTest();
+
+  //ops:
+  for (let i = 0; i < config.Ops.length; i++) {
+
+    if (config.Ops[i].op_name == "setkey") {
+      nonce_signer[`${config.Ops[i].calleraccountIndex}`] = 0;
     }
-    const nonce_signer: nonce = {};
+    await GenerateInput(config.Ops[i].op_name, config.Ops[i], nonce_signer[`${config.Ops[i].calleraccountIndex}`], storage, util);
+    nonce_signer[`${config.Ops[i].calleraccountIndex}`]++;
+  }
 
-    //crypto
-    const util = await getCryptoUtil();
-
-    //pre-test
-    console.log('Compiling Circom');
-    await CreateResultFile();
-    await preTest();
-  
-    //ops:
-    for(let i = 0; i<config.Ops.length;i++){
-      
-      if(config.Ops[i].op_name == "setkey"){
-          nonce_signer[`${config.Ops[i].calleraccountIndex}`] = 0;
-      }
-      await GenerateInput(config.Ops[i].op_name, config.Ops[i], nonce_signer[`${config.Ops[i].calleraccountIndex}`], storage, util);
-      nonce_signer[`${config.Ops[i].calleraccountIndex}`]++;
-    }
-
-    await storage.endSnapshot();
+  await storage.endSnapshot();
 }
 main();
