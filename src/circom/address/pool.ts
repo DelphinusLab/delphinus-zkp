96a9828675a68ad5a91ebac29d5e53223d6fd1e4
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-05-26 12:10:06 +1000

    new algorithm test

--- src/circom/address/pool.ts
+++ src/circom/address/pool.ts
@@ -1,6 +1,7 @@
 import { Field } from "delphinus-curves/src/field";
 import { AddressSpace, getSpaceIndex, toNumber } from "./space";
 import { MerkleTree, PathInfo } from "delphinus-curves/src/merkle-tree-large";
+import BN from "bn.js";
 export class Pool  {
   index: number | Field;
   info_index: number;
--- tests/address/account.test.ts
+++ tests/address/account.test.ts
@@ -2,6 +2,7 @@ import { Field } from "delphinus-curves/src/field";
 import BN from "bn.js";
 import { L2Storage } from "../../src/circom/address-space";
 import { Account } from "../../src/circom/address/account"
+import { Pool } from "../../src/circom/address/pool"
 import { SetKeyCommand } from "../../src/circom/ops/setkey";
 import { AddPoolCommand } from "../../src/circom/ops/addpool";
 
@@ -25,6 +26,58 @@ describe("test account class", () => {
         const leafNode = await storage.getLeave(shareInfoIndex);
 
         expect(leafNode.v).toEqual(new Field(amount0.v.toNumber() + amount1.v.toNumber()).v);
+    });
+
+    test("test getAndUpdateNewShare normal number", async () => {
+        jest.setTimeout(60000); //1 minute timeout
+        let storage: L2Storage = new L2Storage(true);
+
+        const accountIndex = 0;
+        const poolIndex = 0;
+        const amount_supply = new Field(1000);
+        const amount_retrieve =  new Field(0).sub(new Field(500));
+        const account = new Account(storage, accountIndex);
+        const pool = new Pool(storage, poolIndex);
+        const sharePriceKIndex = await pool.getSharePriceKIndex();
+        const k = new Field(10 ** 12);
+        await pool.initSharePriceK(k);
+        
+        await account.getAndUpdateNewShare(
+            poolIndex,
+            sharePriceKIndex,
+            amount_supply
+        );
+
+        await account.getAndUpdateNewShare(
+            poolIndex,
+            sharePriceKIndex,
+            amount_retrieve
+        );
+
+        const shareInfoIndex = await account.getShareInfoIndex(poolIndex);
+        const leafNode = await storage.getLeave(shareInfoIndex);
+        const share_0 = amount_supply.mul(k.sub(new Field(1)));
+        const share_1 = share_0.add(amount_retrieve.mul(k.sub(new Field(1))));
+        const ans = leafNode.sub(share_1);
+
+        expect(ans).toEqual(new Field(0));
+    });
+
+    test("getSharePriceK Error Catch", async () => {
+        jest.setTimeout(60000); //1 minute timeout
+        let storage: L2Storage = new L2Storage(true);
+
+        const accountIndex = 0;
+        const poolIndex = 0;
+        const account = new Account(storage, accountIndex);
+        const pool = new Pool(storage, poolIndex);
+        const sharePriceKIndex = await pool.getSharePriceKIndex();
+
+        async function aaa () {
+            await account.getSharePriceK(sharePriceKIndex);
+        }
+        expect(aaa
+        ).toThrow();
     });
 
     test("test getAndAddShare 18 wei number", async () => {
