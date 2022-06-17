c761ab00aa51bdc0a912bf1538318f0122ae2472
RuilaiZhang <Ray.zhangruilai@gmail.com>
2022-06-17 12:20:25 +1000

    update ts

--- src/circom/ops/addpool.ts
+++ src/circom/ops/addpool.ts
@@ -35,8 +35,8 @@ export class AddPoolCommand extends Command {
     // circuits: check leafValues[0] and leafValues[1] equal to 0
     path.push(await pool.getAndInitTokenIndexAndLiq(tokenIndex0, tokenIndex1, new Field(0), new Field(0)));
 
-    // STEP3: init pool's sharePriceK and remainder
-    path.push(await pool.getAndUpdateKAndRem(new Field(initSharePriceKBN), new Field(0)));
+    // STEP3: init pool's share_total
+    path.push(await pool.getAndInitShareTotal(new Field(0)));
 
     return path;
   }
