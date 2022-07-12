4918cfbbc0e44a55b50a1a05a6351ebdcad3996a
yymone <yymone84@hotmail.com>
2022-07-12 23:38:54 +1000

    CROS-37 frame

--- circom/tools/setup.sh
+++ circom/tools/setup.sh
@@ -1,9 +1,16 @@
-#snarkjs powersoftau contribute pot20_0000.ptau pot20_0001.ptau --name="Delphinus" -v
-npx snarkjs powersoftau new bn128 20 pot20_0000.ptau -v
-npx snarkjs powersoftau contribute pot20_0000.ptau pot20_0001.ptau --name="Second Delphinus contribution" -v -e="mcisanJJNDUINisudajdno@$#@%@#$%#$@%nfisdanSAMpo"
-npx snarkjs powersoftau verify pot20_0001.ptau
-npx snarkjs powersoftau prepare phase2 pot20_0001.ptau pot20_final.ptau -v
+PTAUNUM=23;
+if [ ! -z $1 ]
+then
+    PTAUNUM=$1
+fi
 
-# proof and verify
-# snarkjs groth16 prove main_0000.zkey witness.wtns proof.json public.json
-# snarkjs groth16 verify verification_key.json public.json proof.json
+echo "PATUNUM is $1"
+
+if [ ! -f pot${1}_final.ptau ]
+then
+    npx snarkjs powersoftau new bn128 ${1} pot${1}_0000.ptau -v
+    npx snarkjs powersoftau contribute pot${1}_0000.ptau pot${1}_0001.ptau --name="Second Delphinus contribution" -v -e="mcisanJJNDUINisudajdno@$#@%@#$%#$@%nfisdanSAMpo"
+    npx snarkjs powersoftau verify pot${1}_0001.ptau
+    npx snarkjs powersoftau prepare phase2 pot${1}_0001.ptau pot${1}_final.ptau -v
+fi
+
---
+++ run_unittest.sh
@@ -0,0 +1,18 @@
+# Run ts unittests
+echo "Begin Typescript unittests."
+npx jest --verbose
+echo "Typescript unittests finished."
+
+# Run circom unit test
+echo "Begin Circom unittests"
+cd circom
+
+## Check pot23_final.ptau exists, if not, run setup.sh
+if [ ! -f pot23_final.ptau ]
+    bash setup.sh
+fi
+
+## Run the shell script to run all tests under CircomTestTool tests directory
+## Haven't done
+
+echo "Circom unittests finished"
